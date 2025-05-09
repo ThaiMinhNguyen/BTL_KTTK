b934b5a0c5ac4c4a862470487e0306973ae99c75
MINH2201 <thaiminhnguyen2003@gmail.com>
2025-05-09 13:01:28 +0700

    delete cmt

--- src/main/java/com/nemo/btl_kttk/dao/UserDAO.java
+++ src/main/java/com/nemo/btl_kttk/dao/UserDAO.java
@@ -4,8 +4,6 @@ import com.nemo.btl_kttk.models.User;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
 
 public class UserDAO extends DAO {
     
